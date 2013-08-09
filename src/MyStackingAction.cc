794b5cd31b0a9b0a18b8fedd00b6abc730b5dbfc
wuchen <wuchen1106@gmail.com>
2013-08-10 04:30:35 +0900

    update

--- src/MyStackingAction.cc
+++ src/MyStackingAction.cc
@@ -99,6 +99,8 @@ MyStackingAction::ClassifyNewTrack(const G4Track* aTrack) {
 			aClassification = fKill;
 		}
 	}
+	G4double globalT=aTrack->GetGlobalTime();//Time since the event in which the track belongs is created
+	if (globalT>100*ns) aClassification = fKill;
 
     return aClassification;
 }
