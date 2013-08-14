c5182dd5452859b1af7ef5e5f617626d14ef5cc7
wuchen <wuchen1106@gmail.com>
2013-08-15 06:35:12 +0900

    FEATURE: {MyAnalysisSvc*} now we can set time window for tracking

--- src/MyAnalysisSvcMessenger.cc
+++ src/MyAnalysisSvcMessenger.cc
@@ -17,38 +17,51 @@
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-MyAnalysisSvcMessenger::MyAnalysisSvcMessenger(MyAnalysisSvc* myAnalysisSvc)
+	MyAnalysisSvcMessenger::MyAnalysisSvcMessenger(MyAnalysisSvc* myAnalysisSvc)
 :m_myAnalysisSvc(myAnalysisSvc)
 {
 
-  myAnalysisSvcDir = new G4UIdirectory("/g4sim/myAnalysisSvc/");
-  myAnalysisSvcDir->SetGuidance("myAnalysisSvc control");
+	myAnalysisSvcDir = new G4UIdirectory("/g4sim/myAnalysisSvc/");
+	myAnalysisSvcDir->SetGuidance("myAnalysisSvc control");
 
 	//about output
-  out_card_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_out_card",this);
-  out_card_cmd->SetGuidance("set name for MyAnalysisSvc file");
-  out_card_cmd->SetParameterName("out_card",false);
-  out_card_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+	out_card_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_out_card",this);
+	out_card_cmd->SetGuidance("set name for MyAnalysisSvc file");
+	out_card_cmd->SetParameterName("out_card",false);
+	out_card_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
 
-  //about root file
-  ofile_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_ofile_name",this);
-  ofile_name_cmd->SetGuidance("set name for MyAnalysisSvc file");
-  ofile_name_cmd->SetParameterName("ofile_name",true);
-  ofile_name_cmd->SetDefaultValue("raw_g4sim.root");
-  ofile_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+	//about root file
+	ofile_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_ofile_name",this);
+	ofile_name_cmd->SetGuidance("set name for MyAnalysisSvc file");
+	ofile_name_cmd->SetParameterName("ofile_name",true);
+	ofile_name_cmd->SetDefaultValue("raw_g4sim.root");
+	ofile_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
 
-  tree_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_tree_name",this);
-  tree_name_cmd->SetGuidance("set name for MyAnalysisSvc tree");
-  tree_name_cmd->SetParameterName("tree_name",true);
-  tree_name_cmd->SetDefaultValue("t");
-  tree_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+	tree_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_tree_name",this);
+	tree_name_cmd->SetGuidance("set name for MyAnalysisSvc tree");
+	tree_name_cmd->SetParameterName("tree_name",true);
+	tree_name_cmd->SetDefaultValue("t");
+	tree_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
 
-  //about log
-  run_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_run_name",this);
-  run_name_cmd->SetGuidance("set run_name");
-  run_name_cmd->SetParameterName("run_name",true);
-  run_name_cmd->SetDefaultValue("test");
-  run_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+	//about cut
+	set_minT_cmd = new G4UIcmdWithADoubleAndUnit("/g4sim/myAnalysisSvc/set_minT",this);
+	set_minT_cmd->SetGuidance("Define minimal time");
+	set_minT_cmd->SetParameterName("minT",false,false);
+	set_minT_cmd->SetDefaultUnit("ns");
+	set_minT_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+
+	set_maxT_cmd = new G4UIcmdWithADoubleAndUnit("/g4sim/myAnalysisSvc/set_maxT",this);
+	set_maxT_cmd->SetGuidance("Define minimal time");
+	set_maxT_cmd->SetParameterName("maxT",false,false);
+	set_maxT_cmd->SetDefaultUnit("ns");
+	set_maxT_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+
+	//about log
+	run_name_cmd = new G4UIcmdWithAString("/g4sim/myAnalysisSvc/set_run_name",this);
+	run_name_cmd->SetGuidance("set run_name");
+	run_name_cmd->SetParameterName("run_name",true);
+	run_name_cmd->SetDefaultValue("test");
+	run_name_cmd->AvailableForStates(G4State_PreInit,G4State_Idle);
 
 }
 
@@ -58,14 +71,18 @@ MyAnalysisSvcMessenger::~MyAnalysisSvcMessenger()
 {
 	delete myAnalysisSvcDir;
 	//about output
-  delete out_card_cmd;
+	delete out_card_cmd;
 
-  //about root file
-  delete ofile_name_cmd;
-  delete tree_name_cmd;
+	//about root file
+	delete ofile_name_cmd;
+	delete tree_name_cmd;
 
-  //about log
-  delete run_name_cmd;
+	//about cut
+	delete set_minT_cmd;
+	delete set_maxT_cmd;
+
+	//about log
+	delete run_name_cmd;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -73,14 +90,18 @@ MyAnalysisSvcMessenger::~MyAnalysisSvcMessenger()
 void MyAnalysisSvcMessenger::SetNewValue(G4UIcommand* command,G4String newValue)
 {      
 	//about output
-  if(command==out_card_cmd){m_myAnalysisSvc->set_out_card(newValue);}
+	if(command==out_card_cmd){m_myAnalysisSvc->set_out_card(newValue);}
 
-  //about root file
-  if(command==ofile_name_cmd){m_myAnalysisSvc->set_ofile_name(newValue);}
-  if(command==tree_name_cmd){m_myAnalysisSvc->set_tree_name(newValue);}
+	//about root file
+	if(command==ofile_name_cmd){m_myAnalysisSvc->set_ofile_name(newValue);}
+	if(command==tree_name_cmd){m_myAnalysisSvc->set_tree_name(newValue);}
 
-  //about log
-  if(command==run_name_cmd){m_myAnalysisSvc->set_run_name(newValue);}
+	//about cut
+    if(command==set_minT_cmd) m_myAnalysisSvc->set_minT(set_minT_cmd->GetNewDoubleValue(newValue));
+    if(command==set_maxT_cmd) m_myAnalysisSvc->set_maxT(set_maxT_cmd->GetNewDoubleValue(newValue));
+
+	//about log
+	if(command==run_name_cmd){m_myAnalysisSvc->set_run_name(newValue);}
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
