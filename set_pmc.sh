dbd60ab5ddbdc55bd78f21949434a37b1536d011
wuchen <wuchen1106@gmail.com>
2013-12-29 15:33:10 +0800

    update

--- set_pmc.sh
+++ set_pmc.sh
@@ -3,13 +3,16 @@
 #for detector construction
 export MATERIALLISTROOT=material_list
 #export GEOCARDROOT=geometry_A9_2p4
+#export GEOCARDROOT=geometry_A9_2p4_4v
+#export GEOCARDROOT=geometry_A9_2p4_0731
+#export GEOCARDROOT=geometry_A9_2p4_0731_4v
 #export GEOCARDROOT=geometry_A9_1p5
 #export GEOCARDROOT=geometry_A9_1p5_4v
 #export GEOCARDROOT=geometry_A9_1p5_0731
 export GEOCARDROOT=geometry_A9_1p5_0731_4v
 #for magField
-export MAGFIELDCARDROOT=MagField_A9_130927
-#export MAGFIELDCARDROOT=MagField_A9_130731
+#export MAGFIELDCARDROOT=MagField_A9_130927
+export MAGFIELDCARDROOT=MagField_A9_130731
 #for generator
 export GENFILEROOT=gen_pmc
 #for output
