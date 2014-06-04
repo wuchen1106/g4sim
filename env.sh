4d5042bffbc2801a9f0a2d178ab489977e785a3d
wuchen <wuchen1106@gmail.com>
2014-06-04 18:22:50 +0900

    forgot to change G4EXEC

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ if [ -z $MYG4SIMROOT ]; then
 	export MYG4SIMROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
 if [ -z $G4SIMEXEC ]; then
-	export G4SIMEXEC=$MYG4SIMROOT/g4sim
+	export G4SIMEXEC=$MYG4SIMROOT/bin/g4sim
 fi
 if ! echo $LD_LIBRARY_PATH | grep -q "$MYG4SIMROOT"; then
 	export LD_LIBRARY_PATH=$MYG4SIMROOT/lib:$LD_LIBRARY_PATH
