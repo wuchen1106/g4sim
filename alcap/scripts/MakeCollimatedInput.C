f16b19f2431898a0ee0a920efbef763ec57d9690
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-10-27 17:03:02 +0000

    AE: The script to create the necessary histograms for the collimated input mode

--- alcap/scripts/MakeCollimatedInput.C
+++ alcap/scripts/MakeCollimatedInput.C
@@ -1,96 +1,113 @@
 void MakeCollimatedInput() {
 
-  TFile* input = new TFile("collimator-monitor.root", "READ");
+  TFile* input = new TFile("output/raw_g4sim.root", "READ");
   TTree* tree = (TTree*) input->Get("tree");
   int n_entries = =tree->GetEntries();
 
-  TH3F* hMomentum = new TH3F("hMomentum", "hMomentum", 100,-5,5, 100,-5,5, 350,0,35);
+  TH3F* hPxPyPz = new TH3F("hPxPyPz", "hPxPyPz", 100,-5,5, 100,-5,5, 350,0,35);
+  TH3F* hXPxPz = new TH3F("hXPxPz", "hXPxPz", 80,-4,4, 100,-5,5, 350,0,35);
+  TH3F* hYPyPz = new TH3F("hYPyPz", "hYPyPz", 80,-4,4, 100,-5,5, 350,0,35);
+  tree->Draw("M_pz*1000:M_py*1000:M_px*1000>>hPxPyPz", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
+  tree->Draw("M_pz*1000:M_px*1000:M_x>>hXPxPz", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
+  tree->Draw("M_pz*1000:M_py*1000:M_y>>hYPyPz", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
+
   TH3F* hPosition = new TH3F("hPosition", "hPosition", 80,-4,4, 80,-4,4, 10,-8,-7);
-  tree->Draw("M_pz*1000:M_py*1000:M_px*1000>>hMomentum", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
   tree->Draw("M_z:M_y:M_x>>hPosition", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
 
-  TH1F* hOriginalPX = new TH1F("hOriginalPX", "hOriginalPX", 100,-5,5);
-  TH1F* hOriginalPY = new TH1F("hOriginalPY", "hOriginalPY", 100,-5,5);
-  TH1F* hOriginalPZ = new TH1F("hOriginalPZ", "hOriginalPZ", 350,0,35);
-  tree->Draw("M_px*1000>>hOriginalPX", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
-  tree->Draw("M_py*1000>>hOriginalPY", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
-  tree->Draw("M_pz*1000>>hOriginalPZ", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
+  TH2F* hXYTree = new TH2F("hXYTree", "hXYTree", 80,-4,4, 80,-4,4);
+  TH2F* hXYRandom = new TH2F("hXYRandom", "hXYRandom", 80,-4,4, 80,-4,4);
+  tree->Draw("M_y:M_x>>hXYTree", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
 
-  TH1F* hRandomPX = new TH1F("hRandomPX", "hRandomPX", 100,-5,5);
-  TH1F* hRandomPY = new TH1F("hRandomPY", "hRandomPY", 100,-5,5);
-  TH1F* hRandomPZ = new TH1F("hRandomPZ", "hRandomPZ", 350,0,35);
-  hRandomPX->SetLineColor(kRed);
-  hRandomPY->SetLineColor(kRed);
-  hRandomPZ->SetLineColor(kRed);
+  TH2F* hPxYTree = new TH2F("hPxYTree", "hPxYTree", 100,-5,5, 80,-4,4);
+  TH2F* hPxYRandom = new TH2F("hPxYRandom", "hPxYRandom", 100,-5,5, 80,-4,4);
+  tree->Draw("M_y:M_px*1000>>hPxYTree", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
 
+  TH2F* hPyXTree = new TH2F("hPyXTree", "hPyXTree", 100,-5,5, 80,-4,4);
+  TH2F* hPyXRandom = new TH2F("hPyXRandom", "hPyXRandom", 100,-5,5, 80,-4,4);
+  tree->Draw("M_x:M_py*1000>>hPyXTree", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
 
-  TH1F* hOriginalX = new TH1F("hOriginalX", "hOriginalX", 80,-4,4);
-  TH1F* hOriginalY = new TH1F("hOriginalY", "hOriginalY", 80,-4,4);
-  TH1F* hOriginalZ = new TH1F("hOriginalZ", "hOriginalZ", 10,-8,-7);
-  tree->Draw("M_x>>hOriginalX", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
-  tree->Draw("M_y>>hOriginalY", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
-  tree->Draw("M_z>>hOriginalZ", "M_particleName == \"mu-\" && M_volName == \"ColMon\"");
+  /*  double px, py, pz;
+  for (int i = 0; i < hPxPyPz->GetEntries(); ++i) {
+    bool found = false;
+    while (!found) {
+      hPxPyPz->GetRandom3(px, py, pz);
+    
+      // First get the slice in the X-Z plane
+      TH3F* hist_for_x = hXPxPz->Clone();
+      int bin = hist_for_x->GetZaxis()->FindBin(pz);
+      hist_for_x->GetZaxis()->SetRange(bin, bin);
+      TH2F* hXPx = hist_for_x->Project3D("xy");
 
-  TH1F* hRandomX = new TH1F("hRandomX", "hRandomX", 80,-4,4);
-  TH1F* hRandomY = new TH1F("hRandomY", "hRandomY", 80,-4,4);
-  TH1F* hRandomZ = new TH1F("hRandomZ", "hRandomZ", 10,-8,-7);
-  hRandomX->SetLineColor(kRed);
-  hRandomY->SetLineColor(kRed);
-  hRandomZ->SetLineColor(kRed);
+      // Now get the slice in the y plane so that we can randomly select a value for x
+      bin = hXPx->GetXaxis()->FindBin(px);
+      hXPx->GetXaxis()->SetRange(bin, bin);
+      TH1D* hX = hXPx->ProjectionY();
 
-  double px, py, pz;
-  double x, y, z;
-  for (int i = 0; i < 2*hMomentum->GetEntries(); ++i) {
-    hMomentum->GetRandom3(px, py, pz);
-    hRandomPX->Fill(px);
-    hRandomPY->Fill(py);
-    hRandomPZ->Fill(pz);
+      // Randomly select a value for x
+      double x = hX->GetRandom();
 
-    hPosition->GetRandom3(x, y, z);
-    hRandomX->Fill(x);
-    hRandomY->Fill(y);
-    hRandomZ->Fill(z);
+      // Delete the hists
+      delete hist_for_x;
+      delete hXPx;
+      delete hX;
+
+      // Now do the same of the y axis
+      // First get the slice in the Y-Z plane
+      TH3F* hist_for_y = hYPyPz->Clone();
+      bin = hist_for_y->GetZaxis()->FindBin(pz);
+      hist_for_y->GetZaxis()->SetRange(bin, bin);
+      TH2F* hYPy = hist_for_y->Project3D("xy");
+
+      // Now get the slice in the y plane so that we can randomly select a value for y
+      bin = hYPy->GetXaxis()->FindBin(py);
+      //    std::cout << "bin: " << bin << std::endl;
+      hYPy->GetXaxis()->SetRange(bin, bin);
+      TH1D* hY = hYPy->ProjectionY();
+
+      // Randomly select a value for y
+      double y = hY->GetRandom();
+
+      // Delete the hists
+      delete hist_for_y;
+      delete hYPy;
+      delete hY;
+
+      // Track back to make sure we did pass ellipse
+      double delta = 2; // monitor is 20mm in front of hole
+      double p_total = std::sqrt(px*px + py*py + pz*pz);
+
+      double unit_px = px / p_total;
+      double trackback_x = x - unit_px*delta;
+      double unit_py = py / p_total;
+      double trackback_y = y - unit_py*delta;
+
+      double ellipse = (trackback_x*trackback_x)/(1.6*1.6) + (trackback_y*trackback_y)/(2.5*2.5);
+      //      std::cout << x << ", " << y << " = " << ellipse << "?" << std::endl;
+      if (ellipse <= 1) {
+	//	std::cout << ellipse << " OK" << std::endl;
+	found = true;
+      }
+    }
+
+    hXYRandom->Fill(x, y);
+    hPxYRandom->Fill(px, y);
+    hPyXRandom->Fill(py, x);
+    //    std::cout << "(px, py, pz) = (" << px << ", " << py << ", " << pz << ")" << std::endl;
+    //    std::cout << "(x, y) = (" << x << ", " << y << ")" << std::endl;
   }
-
-  TCanvas* c_px = new TCanvas("c_px", "c_px");
-  hRandomPX->Scale(0.5);
-  hOriginalPX->Draw();
-  hRandomPX->Draw("SAMES");
-  c_px->Update();
-
-  TCanvas* c_py = new TCanvas("c_py", "c_py");
-  hOriginalPY->Draw();
-  hRandomPY->Scale(0.5);
-  hRandomPY->Draw("SAMES");
-  c_py->Update();
-
-  TCanvas* c_pz = new TCanvas("c_pz", "c_pz");
-  hOriginalPZ->Draw();
-  hRandomPZ->Scale(0.5);
-  hRandomPZ->Draw("SAMES");
-  c_pz->Update();
-
-
-  TCanvas* c_x = new TCanvas("c_x", "c_x");
-  hRandomX->Scale(0.5);
-  hOriginalX->Draw();
-  hRandomX->Draw("SAMES");
-  c_x->Update();
-
-  TCanvas* c_y = new TCanvas("c_y", "c_y");
-  hOriginalY->Draw();
-  hRandomY->Scale(0.5);
-  hRandomY->Draw("SAMES");
-  c_y->Update();
-
-  TCanvas* c_z = new TCanvas("c_z", "c_z");
-  hOriginalZ->Draw();
-  hRandomZ->Scale(0.5);
-  hRandomZ->Draw("SAMES");
-  c_z->Update();
-
-  TFile* output = new TFile("CollimatedInput.root", "CREATE");
-  hMomentum->Write();
+  */
+  TFile* output = new TFile("CollimatedInput.root", "RECREATE");
+  hPxPyPz->Write();
+  hXPxPz->Write();
+  hYPyPz->Write();
   hPosition->Write();
+
+  hXYTree->Write();
+  hXYRandom->Write();
+  hPxYTree->Write();
+  hPxYRandom->Write();
+  hPyXTree->Write();
+  hPyXRandom->Write();
+
   output->Close();
 }
