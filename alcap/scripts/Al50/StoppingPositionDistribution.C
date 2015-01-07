31c92dcbcf217a8fd6bfe2986b44642d874bbf51
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-01-07 15:49:06 +0100

    Want the global stopping positions for use in the MC

--- alcap/scripts/Al50/StoppingPositionDistribution.C
+++ alcap/scripts/Al50/StoppingPositionDistribution.C
@@ -3,13 +3,20 @@ void StoppingPositionDistribution(std::string filename) {
   TFile* file = new TFile(filename.c_str(), "READ");
   TTree* tree = (TTree*) file->Get("tree");
   
-  TH3F* hStoppingPositions = new TH3F("hStoppingPositions", "Positions of Stopped Muons in Target", 10,-5,5, 10,-5,5, 60,-30,30);
-  hStoppingPositions->SetXTitle("Local X [cm]");
-  hStoppingPositions->SetYTitle("Local Y [cm]");
-  hStoppingPositions->SetZTitle("Local Z [#mum]");
-  tree->Draw("M_local_Oz*10000:M_local_Oy:M_local_Ox>>hStoppingPositions", "M_particleName==\"mu-\" && M_volName==\"Target\" && M_stopped==1", "");
+  TH3F* hLocalStoppingPositions = new TH3F("hLocalStoppingPositions", "Positions of Stopped Muons in Target (local coords)", 10,-5,5, 10,-5,5, 60,-30,30);
+  hLocalStoppingPositions->SetXTitle("Local X [cm]");
+  hLocalStoppingPositions->SetYTitle("Local Y [cm]");
+  hLocalStoppingPositions->SetZTitle("Local Z [#mum]");
+  tree->Draw("M_local_Oz*10000:M_local_Oy:M_local_Ox>>hLocalStoppingPositions", "M_particleName==\"mu-\" && M_volName==\"Target\" && M_stopped==1", "");
+
+  TH3F* hGlobalStoppingPositions = new TH3F("hGlobalStoppingPositions", "Positions of Stopped Muons in Target (global coords)", 10,-5,5, 10,-5,5, 60,-30,30);
+  hGlobalStoppingPositions->SetXTitle("Global X [cm]");
+  hGlobalStoppingPositions->SetYTitle("Global Y [cm]");
+  hGlobalStoppingPositions->SetZTitle("Global Z [cm]");
+  tree->Draw("M_Oz:M_Oy:M_Ox>>hGlobalStoppingPositions", "M_particleName==\"mu-\" && M_volName==\"Target\" && M_stopped==1", "");
 
   TFile* out_file = new TFile("output.root", "RECREATE");
-  hStoppingPositions->Write();
+  hLocalStoppingPositions->Write();
+  hGlobalStoppingPositions->Write();
   out_file->Close();
 }
