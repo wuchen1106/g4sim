e7d73f80d62e66e89dac09474a33649ee2d2fdac
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-01-08 10:41:32 +0100

    Fairly extensive refactor of Al50 transfer matrix macro

--- alcap/scripts/Al50/TransferMatrix.C
+++ alcap/scripts/Al50/TransferMatrix.C
@@ -1,3 +1,6 @@
+// NB this assumes you are working on a MC run where protons were the initial particle
+// generated inside the target
+
 #include "TFile.h"
 #include "TTree.h"
 #include "TBranch.h"
@@ -23,23 +26,27 @@ void TransferMatrix(std::string filename) {
   TFile* file = new TFile(filename.c_str(), "READ");
   TTree* tree = (TTree*) file->Get("tree");
   
+  double bin_width = 250;
+  double energy_high = 15000;
+  double energy_low = 0;
+  int n_bins = (energy_high - energy_low) / bin_width;
+
   std::vector<Arm> arms;
   Arm right_arm;
   Arm left_arm;
   right_arm.detname = "SiR";
   right_arm.monname = "ESi1";
   std::string responsename = right_arm.detname + "_response";
-  right_arm.response = RooUnfoldResponse(25,0,10000, responsename.c_str());
+  right_arm.response = RooUnfoldResponse(n_bins,energy_low,energy_high, responsename.c_str());
   left_arm.detname = "SiL";
   left_arm.monname = "ESi2";
   responsename = left_arm.detname + "_response";
-  left_arm.response = RooUnfoldResponse(25,0,10000, responsename.c_str());
+  left_arm.response = RooUnfoldResponse(n_bins,energy_low,energy_high, responsename.c_str());
   arms.push_back(right_arm);
   arms.push_back(left_arm);
 
   std::vector<std::string>* particleName = 0;
   std::vector<std::string>* volName = 0;
-  std::vector<std::string>* ovolName = 0;
   std::vector<double>* edep = 0;
   std::vector<int>* stopped = 0;
   double i_px = 0;
@@ -53,7 +60,6 @@ void TransferMatrix(std::string filename) {
   
   TBranch* br_particleName = tree->GetBranch("M_particleName");
   TBranch* br_volName = tree->GetBranch("M_volName");
-  TBranch* br_ovolName = tree->GetBranch("M_ovolName");
   TBranch* br_edep = tree->GetBranch("M_edep");
   TBranch* br_stopped = tree->GetBranch("M_stopped");
   TBranch* br_i_px = tree->GetBranch("i_px");
@@ -64,11 +70,9 @@ void TransferMatrix(std::string filename) {
   TBranch* br_px = tree->GetBranch("M_px");
   TBranch* br_py = tree->GetBranch("M_py");
   TBranch* br_pz = tree->GetBranch("M_pz");
-
   
   br_particleName->SetAddress(&particleName);
   br_volName->SetAddress(&volName);
-  br_ovolName->SetAddress(&ovolName);
   br_edep->SetAddress(&edep);
   br_stopped->SetAddress(&stopped);
   br_i_px->SetAddress(&i_px);
@@ -81,10 +85,6 @@ void TransferMatrix(std::string filename) {
   br_pz->SetAddress(&pz);
 
   double proton_mass = 938.272*1000; // convert to keV
-  
-  TH2F* hTransfer_SiR = new TH2F("hTransfer_SiR", "hTransfer_SiR", 50,0,10000, 50,0,10000);
-  hTransfer_SiR->SetXTitle("Observed Energy [keV]");
-  hTransfer_SiR->SetYTitle("True Energy [keV]");
   
   int n_entries = tree->GetEntries();
   for (int iEntry = 0; iEntry < n_entries; ++iEntry) {
@@ -102,13 +102,15 @@ void TransferMatrix(std::string filename) {
       double thick_trackID = 0;
       double thin_trackID = 0;
       double thick_time = 0;
-      double thin_time = 0;
       double thick_px = 0;
       double thick_py = 0;
       double thick_pz = 0;
+      double thin_time = 0;
       double thin_px = 0;
       double thin_py = 0;
       double thin_pz = 0;
+      std::vector<Arm>::iterator thick_arm; // not sure which arm the proton will be found in
+      std::vector<Arm>::iterator thin_arm; // not sure which arm the proton will be found in
 
       // Calculate true energy here
       double true_px = i_px*1e3;
@@ -121,76 +123,78 @@ void TransferMatrix(std::string filename) {
       //      std::cout << "Entry #" << iEntry << ": true kinetic energy = " << true_kinetic_energy << std::endl;
 
       for (int iElement = 0; iElement < particleName->size();  ++iElement) {
+
+	std::string i_particleName = particleName->at(iElement);
+	std::string i_volName = volName->at(iElement);
 	
 	// Loop through the arms
 	for (std::vector<Arm>::iterator i_arm = arms.begin(); i_arm != arms.end(); ++i_arm) {
 	  std::string thick_monname = i_arm->monname;
 	  std::string thin_monname = "d"+i_arm->monname;
 
-	  if (particleName->at(iElement) == "proton" && volName->at(iElement) == thick_monname) {
+	  if (i_particleName == "proton" && i_volName == thick_monname) {
 	    thick_hit = true;
 	    E = edep->at(iElement)*1e6; // convert to keV
 	    thick_trackID = tid->at(iElement);
 	    thick_time = t->at(iElement);
+	    thick_arm = i_arm;
 	    thick_px = px->at(iElement)*1e6;
 	    thick_py = py->at(iElement)*1e6;
 	    thick_pz = pz->at(iElement)*1e6;
-	    
-	    if (iEntry == 279485) {
-	      std::cout << "Truth (px, py, pz) = (" << true_px << ", " << true_py << ", " << true_pz << ") keV" << std::endl;
-	      std::cout << "p^2 = " << squared_mom << ", m = " << proton_mass << ", E_t = " << total_energy << std::endl;
-	      std::cout << "Thick E = " << E << std::endl;
-	      std::cout << "Thick t = " << t->at(iElement) << " ns" << std::endl;
-	    }
-	  
 	  }
-	  else if (particleName->at(iElement) == "proton" && volName->at(iElement) == thin_monname) {
+	  else if (i_particleName == "proton" && i_volName == thin_monname) {
 	    thin_hit = true;
 	    dE = edep->at(iElement)*1e6;
 	    thin_trackID = tid->at(iElement);
 	    thin_time = t->at(iElement);
+	    thin_arm = i_arm;
 	    thin_px = px->at(iElement)*1e6;
 	    thin_py = py->at(iElement)*1e6;
 	    thin_pz = pz->at(iElement)*1e6;
-
-	    if (iEntry == 279485) {
-	      std::cout << "Truth (px, py, pz) = (" << true_px << ", " << true_py << ", " << true_pz << ") keV" << std::endl;
-	      std::cout << "p^2 = " << squared_mom << ", m = " << proton_mass << ", E_t = " << total_energy << std::endl;
-	      std::cout << "Thin E = " << dE << std::endl;
-	      std::cout << "Thin t = " << t->at(iElement) << " ns" << std::endl;
-	    }
-	  }
-	
-	
-	  if (thick_hit && thin_hit) { // assuming that the two hits will be next to each other in the tree
-	    double observed_E = E+dE;
-	    if (true_kinetic_energy < observed_E) {
-	      std::cout << "Entry # " << iEntry << ": " << thick_trackID << ", " << thin_trackID << std::endl;
-	      std::cout << "E = " << E << ", dE = " << dE << std::endl;
-	      std::cout << "Thick t = " << thick_time << ", thin t = " << thin_time << std::endl;
-	      std::cout << "Thick p = (" << thick_px << ", " << thick_py << ", " << thick_pz << "), thin p = ("
-			<< thin_px << ", " << thin_py << ", " << thin_pz << ")" << std::endl;
-	      std::cout << "True p = ("<< true_px << ", " << true_py << ", " << true_pz << ")" << std::endl;
-	      std::cout << "That's weird... " << true_kinetic_energy << " < " << observed_E << std::endl;
-	    }
-	    //	  else {
-	    //	    hTransfer_SiR->Fill(observed_E, true_kinetic_energy);
-	    i_arm->response.Fill(observed_E, true_kinetic_energy);
-	    //	  }
-	    thick_hit = false;
-	    thin_hit = false; // reset to false in case there is another proton in this event
-	    E = dE = thick_trackID = thin_trackID = 0;
 	  }
 	}
       }
+      
+      // After looping through the elements, we will hopefully have found the initial proton again
+      if (thick_hit && thin_hit) {
+	// For debugging
+	//	std::cout << "Track IDs (thin, thick) = (" << thin_trackID << ", " << thick_trackID << ")" << std::endl;
+	//	std::cout << "Times  (thin, thick) = (" << thin_time << ", " << thick_time << ")" << std::endl;
+	//	std::cout << "Arms  (thin, thick) = (" << thin_arm->detname << ", " << thick_arm->detname << ")" << std::endl;
+	
+	double observed_E = E+dE;
+	if (true_kinetic_energy < observed_E) {
+	  std::cout << "Entry # " << iEntry << ": " << thick_trackID << ", " << thin_trackID << std::endl;
+	  std::cout << "E = " << E << ", dE = " << dE << std::endl;
+	  std::cout << "Thick t = " << thick_time << ", thin t = " << thin_time << std::endl;
+	  std::cout << "Thick p = ("<< thick_px << ", " << thick_py << ", " << thick_pz << ")" << std::endl;
+	  std::cout << "Thin p = ("<< thin_px << ", " << thin_py << ", " << thin_pz << ")" << std::endl;
+	  std::cout << "True p = ("<< true_px << ", " << true_py << ", " << true_pz << ")" << std::endl;
+	  std::cout << "That's weird... " << true_kinetic_energy << " < " << observed_E << std::endl;
+	}
+	
+	for (std::vector<Arm>::iterator i_arm = arms.begin(); i_arm != arms.end(); ++i_arm) {	
+	  if (i_arm == thick_arm) {
+	    i_arm->response.Fill(observed_E, true_kinetic_energy); // this arm saw the proton
+	  }
+	  else {
+	    i_arm->response.Miss(true_kinetic_energy); // the other one didn't
+	  }
+	}
+      }
+      else {
+	//	std::cout << true_kinetic_energy << " keV missed" << std::endl;
+	for (std::vector<Arm>::iterator i_arm = arms.begin(); i_arm != arms.end(); ++i_arm) {
+	  i_arm->response.Miss(true_kinetic_energy); // both arms missed the proton
+	}
+      }
   }
-  //  hTransfer_SiR->Draw("COLZ");
-
-       TFile* out_file = new TFile("output.root", "RECREATE");
-       for (std::vector<Arm>::iterator i_arm = arms.begin(); i_arm != arms.end(); ++i_arm) {
-	 i_arm->response.Hresponse()->SetXTitle("Observed E [keV]");
-	 i_arm->response.Hresponse()->SetYTitle("True E [keV]");
-	 i_arm->response.Write();
-       }
-       out_file->Close();
+  
+  TFile* out_file = new TFile("output.root", "RECREATE");
+  for (std::vector<Arm>::iterator i_arm = arms.begin(); i_arm != arms.end(); ++i_arm) {
+    i_arm->response.Hresponse()->SetXTitle("Observed E [keV]");
+    i_arm->response.Hresponse()->SetYTitle("True E [keV]");
+    i_arm->response.Write();
+  }
+  out_file->Close();
 }
