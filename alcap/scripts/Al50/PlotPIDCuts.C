18bd1826e9c997264bfe44e9bfe02382d536ada2
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-02-24 14:46:24 +0100

    Make sure that we are reading in correctly£

--- alcap/scripts/Al50/PlotPIDCuts.C
+++ alcap/scripts/Al50/PlotPIDCuts.C
@@ -23,9 +23,23 @@ void PlotPIDCuts() {
     std::string input_filename = "pid-cuts-" + i_arm->armname + ".txt";
     TTree* tree = new TTree();
     tree->ReadFile(input_filename.c_str());
-    tree->Draw("p_stop_mean:energy>>hist", "", "");
-    tree->Draw("proton_mean:energy>>hist2", "", "SAME");
-    tree->Print();
+
+    double energy = 0;
+    TBranch* br_energy = tree->GetBranch("energy");
+    br_energy->SetAddress(&energy);
+
+    double p_stop_mean = 0;
+    TBranch* br_mean = tree->GetBranch("p_stop_mean");
+    br_mean->SetAddress(&p_stop_mean);
+
+    double p_stop_rms = 0;
+    TBranch* br_rms = tree->GetBranch("p_stop_rms");
+    br_rms->SetAddress(&p_stop_rms);
+
+    for (int i_entry = 0; i_entry < tree->GetEntries(); ++i_entry) {
+      tree->GetEntry(i_entry);
+      std::cout << energy << " " << p_stop_mean << " " << p_stop_rms << std::endl;
+    }
   }
 
 }
