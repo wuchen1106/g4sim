a3904c2e2636c634d6ffd44299999c3765ef2402
wuchen <wuchen1106@gmail.com>
2019-06-02 14:14:48 +0900

    Added new field map class

---
+++ include/G4SimpleEMField.hh
@@ -0,0 +1,79 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4SimpleEMField.hh 66356 2012-12-18 09:02:32Z gcosmo $
+//
+//
+// class G4SimpleEMField
+//
+// Class description:
+//
+// Electric Field abstract class, implements inquiry function interface.
+
+// History:
+// - Created. JA, November 4th, 2003.
+
+#ifndef G4SIMPLEEM_FIELD_DEF
+#define G4SIMPLEEM_FIELD_DEF
+
+#include "G4Types.hh"
+#include "G4ElectroMagneticField.hh"
+
+class G4SimpleEMField : public G4ElectroMagneticField
+{
+  public:  // with description
+
+     G4SimpleEMField();
+     G4SimpleEMField(double v);
+     virtual ~G4SimpleEMField();
+       // Constructor and destructor. No actions.
+
+     G4SimpleEMField(const G4SimpleEMField &r);
+     G4SimpleEMField& operator = (const G4SimpleEMField &p);
+       // Copy constructor & assignment operator.
+
+     G4bool   DoesFieldChangeEnergy() const { return true; }
+       // Since an electric field can change track energy
+
+     G4UniformElectricField(const G4ThreeVector FieldVector );
+     // A field with value equal to FieldVector.
+
+     G4UniformElectricField(G4double vField,
+                            G4double vTheta,
+                            G4double vPhi     ) ;
+
+     virtual void  GetFieldValue( const G4double Point[4],G4double *field ) const;
+
+	 void SetEleCylinderVoltageScaled(double v){fEleCylinderVoltageScaled = v;};
+
+  private:
+    
+     G4double fEleCylinderVoltageScaled;
+
+     G4double fFieldComponents[6] ;
+};
+
+#endif /* G4ELECTRIC_FIELD_DEF */
---
+++ src/G4SimpleEMField.cc
@@ -0,0 +1,113 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4SimpleEMField.cc 96678 2016-04-29 16:21:01Z gcosmo $
+//
+// --------------------------------------------------------------------
+
+#include "G4SimpleEMField.hh"
+
+G4SimpleEMField::G4SimpleEMField():
+    fVoltageScaled(0)
+{
+}
+
+G4SimpleEMField::~G4SimpleEMField()
+{
+}
+
+G4SimpleEMField::G4SimpleEMField(const G4SimpleEMField &p)
+  : G4ElectroMagneticField(p)
+{
+    for (G4int i=0; i<6; i++)
+    {
+        fFieldComponents[i] = p.fFieldComponents[i];
+    }
+    fVoltageScaled = p.fVoltageScaled;
+}
+
+G4SimpleEMField::SetEleUniformVector(const G4ThreeVector FieldVector )
+{
+    fFieldComponents[3] += FieldVector.x();
+    fFieldComponents[4] += FieldVector.y();
+    fFieldComponents[5] += FieldVector.z();
+}
+
+G4SimpleEMField::SetEleUniformVector(G4double vField,
+                                     G4double vTheta,
+                                     G4double vPhi    )
+{
+    fFieldComponents[3] += vField*std::sin(vTheta)*std::cos(vPhi) ;
+    fFieldComponents[4] += vField*std::sin(vTheta)*std::sin(vPhi) ;
+    fFieldComponents[5] += vField*std::cos(vTheta) ;
+}
+
+G4SimpleEMField::SetMagUniformVector(const G4ThreeVector FieldVector )
+{
+    fFieldComponents[0] += FieldVector.x();
+    fFieldComponents[1] += FieldVector.y();
+    fFieldComponents[2] += FieldVector.z();
+}
+   
+G4SimpleEMField::SetMagUniformVector(G4double vField,
+                                     G4double vTheta,
+                                     G4double vPhi    )
+{
+    fFieldComponents[0] += vField*std::sin(vTheta)*std::cos(vPhi) ;
+    fFieldComponents[1] += vField*std::sin(vTheta)*std::sin(vPhi) ;
+    fFieldComponents[2] += vField*std::cos(vTheta) ;
+}
+
+G4SimpleEMField& G4SimpleEMField::operator = (const G4SimpleEMField &p)
+{
+  if (&p == this) return *this;
+  fVoltageScaled = p.fVoltageScaled;
+  for (G4int i=0; i<6; i++)
+  {
+    fFieldComponents[i] = p.fFieldComponents[i];
+  }
+  return *this;
+}
+
+void G4SimpleEMField::GetFieldValue( const G4double Point[4],
+        G4double *field ) const{
+
+    // Uniform magnetic field and electric field
+    fieldBandE[0] += fFieldComponents[0] ;
+    fieldBandE[1] += fFieldComponents[1] ;
+    fieldBandE[2] += fFieldComponents[2] ;
+    fieldBandE[3] += fFieldComponents[3] ;
+    fieldBandE[4] += fFieldComponents[4] ;
+    fieldBandE[5] += fFieldComponents[5] ;
+
+    // Cylinder electric field
+    double r = sqrt(Point[0]*Point[0]+Point[1]*Point[1]);
+    double E = fVoltageScaled/r;
+    field[3] += E*Point[0]/r;
+    field[4] += E*Point[1]/r;
+
+    std::cout<<"GetFieldValue "<<Point[0]<<" "<<Point[1]<<" "<<Point[2]<<" = "<<field[0]<<" "<<field[1]<<" "<<field[2]<<field[3]<<" "<<field[4]<<" "<<field[5]<<std::endl;
+}
