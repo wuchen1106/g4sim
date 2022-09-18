8fa0c717c865a8910d8eab13b17261343ede68a0
wuchen <wuchen1106@gmail.com>
2022-09-19 00:11:55 +0900

    Updated the env.sh file

--- env.sh
+++ env.sh
@@ -1,8 +1,5 @@
 export MYG4SIMROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export G4SIMEXEC=$MYG4SIMROOT/bin/g4sim
-if ! echo $LD_LIBRARY_PATH | grep -q "$MYG4SIMROOT"; then
-	export LD_LIBRARY_PATH=$MYG4SIMROOT/lib:$LD_LIBRARY_PATH
-fi
 if ! echo $PATH | grep -q "$MYG4SIMROOT"; then
-	export PATH=$MYG4SIMROOT/bin:$PATH
+	export PATH=$MYG4SIMROOT/build:$PATH
 fi
